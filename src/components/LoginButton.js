0c36a7d36110ff5d9e501551900e7e535b918f2d
Peter <ppapadim12@gmail.com>
2017-09-11 22:31:17 -0400

    added additional styling

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,7 @@ import { Button } from 'semantic-ui-react';
 
 const LoginButton = (props) =>
 
-  <Button color='black' onClick={props.handleLoginClick} scope='user_profile,email,friends_list'>Login With Facebook</Button>
+  <Button color='black' onClick={props.handleLoginClick} scope='user_profile,email,friends_list'></Button>
   // <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true"></div>
 
 export default LoginButton;
