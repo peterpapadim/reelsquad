1c9e68b9d51adff3020d04e91bb2b9347528f3b0
Peter <ppapadim12@gmail.com>
2017-09-12 23:31:43 -0400

    adjusted api endpoints for production

--- src/adapters/UserAdapter.js
+++ src/adapters/UserAdapter.js
@@ -3,8 +3,8 @@ class UserAdapter {
   static create(firstName, lastName, email, userID){
     let header = new Headers
     header.set('Content-Type', 'application/json')
-    // return fetch(`https://reelsquad-api.herokuapp.com/api/v1/users`, {
-    return fetch(`http://localhost:3000/api/v1/users`, {
+    return fetch(`https://reelsquad-api.herokuapp.com/api/v1/users`, {
+    // return fetch(`http://localhost:3000/api/v1/users`, {
       method:"POST",
       headers: header,
       body: JSON.stringify({firstName, lastName, email, userID})
@@ -12,8 +12,8 @@ class UserAdapter {
   }
 
   static show(userID){
-    // return fetch(`https://reelsquad-api.herokuapp.com/api/v1/users/${userID}`)
-    return fetch(`http://localhost:3000/api/v1/users/${userID}`)
+    return fetch(`https://reelsquad-api.herokuapp.com/api/v1/users/${userID}`)
+    // return fetch(`http://localhost:3000/api/v1/users/${userID}`)
   }
 }
 
